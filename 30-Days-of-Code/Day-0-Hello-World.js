543befba460870480c38f327aa2c01d22f5c2579
jen <nguy.jenni@gmail.com>
2022-07-08 23:01:38 -0500

    create Day-2-Operators

--- 30-Days-of-Code/Day-0-Hello-World.js
+++ 30-Days-of-Code/Day-0-Hello-World.js
@@ -28,6 +28,7 @@
 // On the first line, we print the string literal Hello, World.. On the second line, we print the contents of the  variable which, for this sample case, happens to be Welcome to 30 Days of Code!. If you do not print the variable's contents to stdout, you will not pass the hidden test case.
 
 // Answer
+
 function processData(inputString) {
     // This line of code prints the first line of output
     console.log("Hello, World.");
