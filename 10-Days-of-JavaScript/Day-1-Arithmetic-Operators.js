681374b50744694dd0dc19413b858725785e89a6
nguy.jenni@gmail.com <nguy.jenni@gmail.com>
2023-10-18 14:54:57 -0500

    create Day-1-Arithmetic-Operators

---
+++ 10-Days-of-JavaScript/Day-1-Arithmetic-Operators.js
@@ -0,0 +1,14 @@
+// Objective
+
+// In this challenge, we practice using arithmetic operators. Check out the attached tutorial for resources.
+
+// Task
+
+// Complete the following functions in the editor below:
+
+// getArea(length, width): Calculate and return the area of a rectangle having sides  and .
+// getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides  and .
+// The values returned by these functions are printed to stdout by locked stub code in the editor.
+
+// Answer 
+
