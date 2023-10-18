22ab3f537afa2a64237390d500c62d00658adddb
nguy.jenni@gmail.com <nguy.jenni@gmail.com>
2023-10-18 14:37:44 -0500

    create Day-0-Hello-World

---
+++ 10-Days-of-JavaScript/create Day-0-Hello-World.js
@@ -0,0 +1,21 @@
+// Objective
+
+// In this challenge, we review some basic concepts that will get you started with this series. Check out the tutorial to learn more about JavaScript's lexical structure.
+
+// Task
+
+// A greeting function is provided for you in the editor below. It has one parameter, . Perform the following tasks to complete this challenge:
+
+// Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+// Use console.log() to print the contents of  (i.e., the argument passed to main).
+// You've got this!
+
+// Answer
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable)
+}
